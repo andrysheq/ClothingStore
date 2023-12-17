d1cc564b696da04fa0809f7195363e6cb8fb936e
Andrey Kareev <andrey.andreych@bk.ru>
2023-12-17 21:15:27 +0300

    Add auth

--- src/main/java/com/web/auction/security/SecurityConfig.java
+++ src/main/java/com/web/auction/security/SecurityConfig.java
@@ -65,7 +65,6 @@ public class SecurityConfig {
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
-
                 // Make H2-Console non-secured; for debug purposes
                 .and()
                 .csrf()
