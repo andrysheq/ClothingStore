d13ca3a68920e1fde67389716cf95c125506abd9
Andrey Kareev <andrey.andreych@bk.ru>
2024-02-21 12:43:57 +0300

    Изменен дизайн страницы товара

--- src/main/java/com/web/auction/controllers/WishListController.java
+++ src/main/java/com/web/auction/controllers/WishListController.java
@@ -4,12 +4,18 @@ import com.web.auction.data.CartRepository;
 import com.web.auction.data.ProductRepository;
 import com.web.auction.data.UserRepository;
 import com.web.auction.data.WishListRepository;
+import com.web.auction.models.Product;
 import com.web.auction.models.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/wishlist")
@@ -27,8 +33,15 @@ public class WishListController {
     @GetMapping
     public String viewCart(Model model, @AuthenticationPrincipal User user) {
         User currentUser = userRepo.findByUsername(user.getUsername());
+        Map<Product, String> wishListWithPhoto = new HashMap<>();
+        List<Product> wishList = wishListRepository.findByUser(currentUser).getProducts();
+        for (Product product : wishList) {
+            byte[] photoBytes = product.getPhotoOfProduct();
+            String photoBase64 = Base64.getEncoder().encodeToString(photoBytes);
+            wishListWithPhoto.put(product, photoBase64);
+        }
         model.addAttribute("currentUser", currentUser);
-        model.addAttribute("wishListItems", wishListRepository.findByUser(currentUser).getProducts());
+        model.addAttribute("wishListItemsMap",wishListWithPhoto);
         return "wishlist";
     }
 }
